e204cd8556f353888393ed98ac0e4bf37e7fe83e
gleafy <greenleafy@yandex.ru>
2025-11-02 08:20:37 +0400

    created habtis views

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Habit
+from .serializers import HabitSerializer
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Habit.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=False, methods=["get"])
+    def public(self, request):
+        """Список публичных привычек"""
+        habits = Habit.objects.filter(is_public=True)
+        page = self.paginate_queryset(habits)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(habits, many=True)
+        return Response(serializer.data)
