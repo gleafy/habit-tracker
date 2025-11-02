2ab541d6dded3eb288eec34a096fc4def2de23fc
gleafy <greenleafy@yandex.ru>
2025-11-02 10:30:14 +0400

    created users serializers

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password", "password_confirm"]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirm"]:
+            raise serializers.ValidationError({"password": "Пароли не совпадают."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class TelegramLinkSerializer(serializers.Serializer):
+    chat_id = serializers.CharField(max_length=100, required=True)
