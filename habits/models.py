53c8f67ed332a5f233bf0fb1517af64555641345
gleafy <greenleafy@yandex.ru>
2025-11-02 07:35:59 +0400

    created habtis models

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    place = models.CharField(max_length=255)
+    time = models.TimeField()
+    action = models.CharField(max_length=255)
+    is_pleasant = models.BooleanField(default=False)
+    related_habit = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+    frequency = models.PositiveSmallIntegerField(default=1)
+    reward = models.CharField(max_length=255, blank=True, null=True)
+    duration = models.PositiveSmallIntegerField()
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.action} в {self.place}"
