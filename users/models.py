8c0e63d3d39dd89b92800214b91eeecb6d940ba5
gleafy <greenleafy@yandex.ru>
2025-11-02 10:54:14 +0400

    created users models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="telegram")
+    chat_id = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Telegram: {self.user.username}"
